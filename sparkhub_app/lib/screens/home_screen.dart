70660316ab3997c8e84bd9f8a2434daf6ea6a3bc
Cookie <hanlicookie@outlook.com>
2023-06-09 15:02:25 -0700

    redirect to home page

---
+++ sparkhub_app/lib/screens/home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+final FirebaseAuth auth = FirebaseAuth.instance;
+late User? user;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    user = auth.currentUser;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [const Text('Home Page'), Text('User Email: ${user!.email}')],
+      ),
+    );
+  }
+}
