1965d4ab4b3f97ab6a4cbf84041577f6796afd87
Cookie <hanlicookie@outlook.com>
2023-06-10 00:01:47 -0700

    database bug fix
    fixed an error where the uid wasn't the path of the JSON object for the user

--- sparkhub_app/lib/utils/user_db.dart
+++ sparkhub_app/lib/utils/user_db.dart
@@ -3,25 +3,13 @@
 import 'package:firebase_database/firebase_database.dart';
 
 final database = FirebaseDatabase.instance.ref();
-final userProfiles = database.child('userProfiles/');
-final userProfilesId = database.child('userProfiles/id');
-
 
 void storeUserInFirebase(String id, String? name, String? email) {
-    userProfiles.set({
-        'id': id,
-    }).then((value) {
-        print('UserId stored successfully in Firebase');
-    }).catchError((error) {
-        print('Failed to store ID in Firebase: $error');
-    });
+  final userProfiles = database.child('userProfiles/$id');
 
-    userProfilesId.set({
-        'name': name,
-        'email': email
-    }).then((value) {
-        print('User stored successfully in Firebase');
-    }).catchError((error) {
-        print('Failed to store user name or email in Firebase: $error');
-    });
+  userProfiles.set({'name': name, 'email': email}).then((value) {
+    print('User stored successfully in Firebase');
+  }).catchError((error) {
+    print('Failed to store user name or email in Firebase: $error');
+  });
 }
