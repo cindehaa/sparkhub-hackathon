7d17919567065e26391a207f8bb53f631fbea2ef
Cookie <hanlicookie@outlook.com>
2023-06-12 05:57:27 -0700

    Revert "Revert "Merge branch 'main' of https://github.com/cindehaa/sparkhub-hackathon into item-cards""
    This reverts commit 178c0ad2cb3637ce02c24a3cf0c8a941c8df4bd3.

--- sparkhub_app/lib/utils/user_db.dart
+++ sparkhub_app/lib/utils/user_db.dart
@@ -3,25 +3,23 @@
 import 'package:firebase_database/firebase_database.dart';
 
 final database = FirebaseDatabase.instance.ref();
-final userProfiles = database.child('userProfiles/');
-final userProfilesId = database.child('userProfiles/id');
 
-
-void storeUserInFirebase(String id, String? name, String? email) {
-    userProfiles.set({
-        'id': id,
-    }).then((value) {
-        print('UserId stored successfully in Firebase');
-    }).catchError((error) {
-        print('Failed to store ID in Firebase: $error');
-    });
-
-    userProfilesId.set({
-        'name': name,
-        'email': email
-    }).then((value) {
-        print('User stored successfully in Firebase');
-    }).catchError((error) {
-        print('Failed to store user name or email in Firebase: $error');
-    });
+void storeUserInFirebase(
+    String id, String? name, String? email, Function() callback) {
+  final userProfiles = database.child('userProfiles');
+  userProfiles.child(id).once(DatabaseEventType.value).then((value) => {
+        if (!value.snapshot.exists)
+          {
+            userProfiles
+                .child(id)
+                .set({'name': name, 'email': email}).then((value) {
+              print('User stored successfully in Firebase');
+              callback();
+            }).catchError((error) {
+              print('Failed to store user name or email in Firebase: $error');
+            })
+          }
+        else
+          {callback()}
+      });
 }
--- sparkhub_app/lib/widgets/home_screen/profile_page.dart
+++ sparkhub_app/lib/widgets/home_screen/profile_page.dart
@@ -1,10 +1,53 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+
+final database = FirebaseDatabase.instance.ref('userProfiles/');
+FirebaseAuth auth = FirebaseAuth.instance;
+User? currentUser = auth.currentUser;
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Profile Page');
+    return Column(
+      children: [
+        SizedBox(
+          width: 500,
+          child: IndexedStack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.account_circle),
+                      Text('Name: ${currentUser?.displayName}'),
+                      Spacer(),
+                      MaterialButton(
+                        onPressed: () {},
+                        child: const Text('Edit Profile'),
+                      )
+                    ],
+                  ),
+                  Text('Email: ${currentUser?.email}'),
+                  MaterialButton(
+                    onPressed: () {},
+                    child: const Text('Sign Out'),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+        Container(
+          color: Colors.blue,
+          width: 100,
+          height: 100,
+        )
+      ],
+    );
   }
 }
